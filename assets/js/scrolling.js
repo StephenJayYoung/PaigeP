e24b19139f99832fbb93d861e2935665ee972d0b
Stephen Young <stephenjayyoung@gmail.com>
2015-12-04 11:13:02 -0800

    fixed scrollspy

--- assets/js/scrolling.js
+++ assets/js/scrolling.js
@@ -1,34 +1,48 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
 
+(function($) {
+    "use strict"; // Start of use strict
 
-var $ = jQuery.noConflict();
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
 
-		var linkScroll = function(){
-			$("a[data-scrollto]").click(function(){
-				var element = $(this),
-					divScrollToAnchor = element.attr('data-scrollto'),
-					divScrollSpeed = element.attr('data-speed'),
-					divScrollOffset = element.attr('data-offset'),
-					divScrollEasing = element.attr('data-easing'),
-					divScrollHighlight = element.attr('data-highlight');
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '#navbar',
+        offset: 51
+    })
 
-					if( !divScrollSpeed ) { divScrollSpeed = 750; }
-					if( !divScrollOffset ) { divScrollOffset = SEMICOLON.initialize.topScrollOffset(); }
-					if( !divScrollEasing ) { divScrollEasing = 'easeOutQuad'; }
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 
-				$('html,body').stop(true).animate({
-					'scrollTop': $( divScrollToAnchor ).offset().top - Number(divScrollOffset)
-				}, Number(divScrollSpeed), divScrollEasing, function(){
-					if( divScrollHighlight != '' ) {
-						if( $(divScrollToAnchor).find('.highlight-me').length > 0 ) {
-							$(divScrollToAnchor).find('.highlight-me').animate({'backgroundColor': divScrollHighlight}, 300);
-							var t = setTimeout( function(){ $(divScrollToAnchor).find('.highlight-me').animate({'backgroundColor': 'transparent'}, 300); }, 500 );
-						} else {
-							$(divScrollToAnchor).animate({'backgroundColor': divScrollHighlight}, 300);
-							var t = setTimeout( function(){ $(divScrollToAnchor).animate({'backgroundColor': 'transparent'}, 300); }, 500 );
-						}
-					}
-				});
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
 
-				return false;
-			});
-		}
+    // Offset for Main Navigation
+    // $('#mainNav').affix({
+    //     offset: {
+    //         top: 100
+    //     }
+    // })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+})(jQuery); // End of use strict
